2ba81ad0892616f81e67a0a1a5c7aaaefc75e29d
wagnerAJS221 <wagneralex221@gmai.com>
2023-02-27 15:15:54 +0000

    set up of app complete

---
+++ react-classic-porsche/src/components/navBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useState } from 'react'
+
+function navBar() {
+  return (
+    <>
+      <nav className="navBar"></nav>
+    </>
+  )
+}
+
+export default navBar
