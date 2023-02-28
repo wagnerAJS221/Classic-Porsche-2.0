b47b120fc673d11ccfe6c5b6a8061a51099037d5
wagnerAJS221 <wagneralex221@gmai.com>
2023-02-28 11:48:18 +0000

    initial layout plans and some FE

--- react-classic-porsche/src/App.js
+++ react-classic-porsche/src/App.js
@@ -1,11 +1,18 @@
 import './App.css'
 import React from 'react'
+import NavBar from './components/NavBar'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Greetings upon this day</h1>
-    </div>
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" exact />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
