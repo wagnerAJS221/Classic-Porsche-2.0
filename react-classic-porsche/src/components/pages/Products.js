c2fed15dc091e1c92ff3547d2a1875b547e4b3ed
wagnerAJS221 <wagneralex221@gmai.com>
2023-03-03 17:33:36 +0000

    more FE structure added

--- react-classic-porsche/src/components/pages/Products.js
+++ react-classic-porsche/src/components/pages/Products.js
@@ -1,12 +1,63 @@
 import React from 'react'
 import '../../App.css'
+import '../Cards.css'
+import CardItem from '../CardItem'
 
 function Products() {
   return (
     <>
       <h1 className="products">PRODUCTS</h1>
       <div className="car-list">
-        <p>This is a test1234</p>
+        <p>
+          {' '}
+          I will be using the card template to render my "car products" but will
+          likely change it next week to build some fake car data that will then
+          replace the hard coded cards
+        </p>
+        <div className="cards__container">
+          <div className="cards__wrapper">
+            <ul className="cards__items">
+              <CardItem
+                src="../../porscheIMG/p19.jpg"
+                text="German Engineering at its finest"
+                label="Find Perfection"
+                path="/products"
+              />
+              <CardItem
+                src="../../porscheIMG/p4.webp"
+                text="Theres something for everyone"
+                label="Mechanial Beauty's true form"
+                path="/products"
+              />
+            </ul>
+            <ul className="cards__items">
+              <CardItem
+                src="../../porscheIMG/p16.jpg"
+                text="Need it for the Track?"
+                label="Speak to our consultants"
+                path="/services"
+              />
+              <CardItem
+                src="../../porscheIMG/p14.jpg"
+                text="Keeping it classy ?"
+                label="Valet Services"
+                path="/services"
+              />
+              <CardItem
+                src="../../porscheIMG/p15.jpg"
+                text="Finely tuned for your needs"
+                label="See Services"
+                path="/services"
+              />
+              <CardItem
+                src="../../porscheIMG/p3.jpg"
+                text="Read about who classic porsche is as a company"
+                label="AboutUs"
+                path="/aboutUs"
+              />
+            </ul>
+          </div>
+        </div>
       </div>
     </>
   )
--- react-classic-porsche/src/components/pages/Services.js
+++ react-classic-porsche/src/components/pages/Services.js
@@ -1,12 +1,53 @@
 import React from 'react'
 import '../../App.css'
+import CardItem from '../CardItem'
 
 export default function Services() {
   return (
     <>
       <h1 className="services">Services</h1>
       <div className="service-list">
-        <p>service list to go here</p>
+        <div className="cards">
+          <h1>Featured</h1>
+          <div className="cards__container">
+            <div className="cards__wrapper">
+              <ul className="cards__items">
+                <CardItem
+                  src="../../porscheIMG/p16.jpg"
+                  text="Here at Classic Porsche we offer a large amount of 
+                  consulting for classic racing Porsches.Please enquire 
+                  via our email should you be interested "
+                  label="Track fittings and Racing"
+                  path="/services"
+                />
+                <CardItem
+                  src="../../porscheIMG/p22.webp"
+                  text="Classic Porsche garages also offer a Valet service. 
+                  This can be anything for a car wash to a full Valet service and everything in between."
+                  label="Valet Services"
+                  path="/services"
+                />
+                <CardItem
+                  src="../../porscheIMG/p21.jpg"
+                  text="Classic Cars from time to 
+                  time do need a bit of pampering. Should you find yourself look for a 
+                  mechanics and technicians that are experts in Classic Porsches, look no further."
+                  label="Vehichle Service"
+                  path="/services"
+                />
+                <CardItem
+                  src="../../porscheIMG/p20.jpg"
+                  text="Feeling Blue? Why not use the Classic 
+                  Porsches styling department to change any of the interior and 
+                  exterior details to meet your needs ? 
+                  Our design team is world class with excellent experiance."
+                  label="Detailing and styling"
+                  path="/services"
+                />
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
